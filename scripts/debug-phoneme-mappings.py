8252c1a21f201caee2e8a4afb50db111b2045cf6
HeshamAsiry <66654236+HeshamAsiry@users.noreply.github.com>
2026-09-18 04:04:50 +0300

    Inspect repeated-character alignment at 27:36

--- scripts/debug-phoneme-mappings.py
+++ scripts/debug-phoneme-mappings.py
@@ -3,7 +3,7 @@ from quranic_phonemizer import Phonemizer
 
 pm = Phonemizer()
 
-for ref in ("10:1",):
+for ref in ("27:36",):
     print("\n=== " + ref + " ===", flush=True)
     result = pm.phonemize(ref)
 
